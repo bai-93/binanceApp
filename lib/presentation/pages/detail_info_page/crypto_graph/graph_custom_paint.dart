dc6d9518e6d34a119e80ae35385aaff0fd37658d
baiaman apsamatov <baiamanapsamatov@baiamans-MacBook-Pro.local>
2023-11-15 22:04:10 +0600

    fixed graph builder and did set range of data

--- lib/presentation/pages/detail_info_page/crypto_graph/graph_custom_paint.dart
+++ lib/presentation/pages/detail_info_page/crypto_graph/graph_custom_paint.dart
@@ -7,19 +7,21 @@ class GraphCustomPaint extends CustomPainter {
   List<double> percentageData = [];
   List<Offset> coordinates = [];
   List<double> anotherData = [
-    2,
-    10,
-    11,
-    15,
-    13,
-    2,
-    20,
-    15,
-    7,
-    10,
-    18,
-    9,
-    5,
+    63006.5,
+    63094.97,
+    62740.13,
+    62734.97,
+    63460.04,
+    62773.37,
+    63195.48,
+    62653.63,
+    62384.96,
+    62743.44,
+    63014.9,
+    63188.83,
+    62979.91,
+    62806.18,
+    63806.18,
   ];
   GraphCustomPaint(this.model) {
     settingsModel();
@@ -68,13 +70,13 @@ class GraphCustomPaint extends CustomPainter {
               nextStepX + stepWidth,
               ((percentageData[i + 1]) * limitSize.height));
         } else {
-          double temp = percentageData[i] < percentageData[i - 1] ? 50.0 : 25.0;
+          double temp = percentageData[i] < percentageData[i - 1] ? 0.0 : 0.0;
           double controlPoint =
-              percentageData[i] < percentageData[i - 1] ? -5.0 : 5.0;
+              percentageData[i] < percentageData[i - 1] ? 0.0 : 0.0;
           path.cubicTo(
-              nextStepX + stepWidth / 2,
+              nextStepX - stepWidth,
               (((percentageData[i]) * limitSize.height + (controlPoint))),
-              nextStepX + stepWidth / 2,
+              nextStepX + stepWidth,
               (percentageData[i]) * limitSize.height + (controlPoint),
               nextStepX + stepWidth,
               ((percentageData[i]) * limitSize.height + (temp)));
@@ -109,36 +111,26 @@ class GraphCustomPaint extends CustomPainter {
   }
 
   void settingsModel() {
+    int startIndex = model.data.length - 15;
+    int endIndex = model.data.length;
+    // here I had to be shortened here, because we got a huge size of data
+    List<CryptoHistoryPriceModel> cryptoHistoryModel =
+        model.data.getRange(startIndex, endIndex).toList();
+    anotherData =
+        cryptoHistoryModel.map((e) => double.parse(e.priceUsd)).toList();
     double majorMaxPrice = anotherData
         .reduce((value, element) => value > element ? value : element);
-    int majorMaxIndex = anotherData.indexOf(majorMaxPrice);
-    anotherData[majorMaxIndex] = 0.0;
-    double minorMaxPrice = anotherData
-        .reduce((value, element) => value > element ? value : element);
-    int minorMaxIndex = anotherData.indexOf(minorMaxPrice);
-    anotherData[majorMaxIndex] = majorMaxPrice;
     List<double> subtractedData =
         anotherData.map((e) => majorMaxPrice - e).toList();
     double maxValueFromSubtracted = subtractedData
         .reduce((value, element) => value > element ? value : element);
-    int index = 0;
     percentageData = subtractedData.map((e) {
       if (e == 0.0) {
-        index += 1;
         return 0.0;
-      }
-      if (index == minorMaxIndex) {
-        print("yes");
-        index++;
-        return 1 - (minorMaxPrice / majorMaxPrice);
       } else if (e == maxValueFromSubtracted) {
-        print(
-            "index == ${index} subtracted Max value  == ${maxValueFromSubtracted}");
-        index++;
-        return e / majorMaxPrice;
+        return 0.9;
       } else {
-        index++;
-        return e / maxValueFromSubtracted;
+        return (e / maxValueFromSubtracted - 0.1).abs();
       }
     }).toList();
   }
@@ -148,59 +140,3 @@ class GraphCustomPaint extends CustomPainter {
     return true;
   }
 }
-
-final data = [
-  83396.89,
-  82364.65,
-  81545.52,
-  82169.63,
-  81390.36,
-  81591.06,
-  81923.55,
-  81500.0,
-  81050.0,
-  81257.9,
-  81385.01,
-  82028.57,
-  81845.01,
-  82040.0,
-  82267.56,
-  82249.01,
-  81891.57,
-  81969.37,
-  81780.0,
-  81990.01,
-  81529.27,
-  80799.75,
-  82615.06,
-  82540.25
-];
-
-final data2 = [1550.0, 1000, 1200, 1300, 1400];
-final data3 = [10, 11, 15, 13, 5, 20, 16, 2, 7, 18, 9, 5, 3];
-final data4 = [
-  62634.79,
-  63027.62,
-  63231.06,
-  63174.99,
-  63000.0,
-  63084.5,
-  62575.85,
-  62799.99,
-  63239.31,
-  62834.99,
-  63006.5,
-  63094.97,
-  62740.13,
-  62734.97,
-  63460.04,
-  62773.37,
-  63195.48,
-  62653.63,
-  62384.96,
-  62743.44,
-  63014.9,
-  63188.83,
-  62979.91,
-  62806.18
-];
