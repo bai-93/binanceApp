dd0f19e95d9a59ce74fcb8a0d87c8f998fc132e4
baiaman apsamatov <baiamanapsamatov@baiamans-MacBook-Pro.local>
2024-03-12 23:35:01 +0600

    removed unusable lines

--- lib/presentation/pages/home_page/settings/settings_table_delegate.dart
+++ lib/presentation/pages/home_page/settings/settings_table_delegate.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:sheker/presentation/pages/home_page/settings/settings.dart';
-
 abstract class SettingsDelegateActionsHandle {
   void selectedRows(String data, int indexSection, int index);
   void selectedTheme(bool isDark);
