c15f1e1760e8d2d899b684daa49ac4b6eca59cc3
baiaman apsamatov <baiamanapsamatov@baiamans-MacBook-Pro.local>
2024-05-23 19:01:46 +0600

    totally completed portfolio list of coins

--- lib/presentation/pages/home_page/portfolio/portfolio_main.dart
+++ lib/presentation/pages/home_page/portfolio/portfolio_main.dart
@@ -5,6 +5,8 @@ import 'package:sheker/presentation/pages/home_page/portfolio/portfolio_content.
 import 'package:sheker/presentation/pages/home_page/portfolio/portfolio_model.dart';
 import 'package:sheker/utilities/shimmer_utility.dart';
 
+import '../../../../utilities/app_colors.dart';
+
 class PortfolioMain extends StatelessWidget {
   PortfolioModel? _model;
   PortfolioMain({super.key});
@@ -61,20 +63,51 @@ class PortfolioMain extends StatelessWidget {
 
   Widget makeCoinsListView(BuildContext context) {
     int coefficient = _model?.getCountOfData() ?? 1;
-    return SizedBox(
-      height: 85.0 * coefficient,
-      child: ListView.separated(
-        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: _model?.getCountOfData() ?? 0,
-        itemBuilder: (context, index) {
-          return PortfolioCellContent(_model?.getCoinData()[index]);
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const SizedBox(
-            height: 8.0,
-          );
-        },
+    return Column(children: [
+      const SizedBox(
+        height: 16.0,
+      ),
+      makeTitleOfPortfolio(context),
+      const SizedBox(
+        height: 8.0,
+      ),
+      SizedBox(
+        height: 85.0 * coefficient,
+        child: ListView.separated(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: _model?.getCountOfData() ?? 0,
+          itemBuilder: (context, index) {
+            return PortfolioCellContent(_model?.getCoinData()[index]);
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return const SizedBox(
+              height: 8.0,
+            );
+          },
+        ),
+      ),
+    ]);
+  }
+
+  Widget makeTitleOfPortfolio(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Portfolio',
+            style: Theme.of(context).textTheme.labelMedium,
+          ),
+          Text(
+            'More',
+            style: TextStyle(
+                color: AppColorsUtility.onboardingPrimary,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w500),
+          )
+        ],
       ),
     );
   }
